6d1781c10d138e8e8d8b58bc3818a8335d682ad1
aavishkarmishra <aavishkarmishra@gmail.com>
2022-07-25 17:36:53 +0530

    Done

---
+++ src/pages/signin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+export const SignIn = () => {
+  return (
+    <div class="container hero">
+      <div class="card signin">
+        <div class="card-body">
+          <h5 class="card-title">Sign In</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Use your account for sign in.</h6>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/signup.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const SignUp = () => {
+  return (
+    <div class="container hero">
+      <div class="card signin">
+        <div class="card-body">
+          <h5 class="card-title">Sign Up</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Create new account.</h6>
+        </div>
+      </div>
+    </div>
+  );
+};
